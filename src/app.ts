4810f44951995c4241bc3b48f04a6fa6e2f50a15
Vinicius <eovinicius10@gmail.com>
2023-11-19 19:58:38 -0300

    feat: add middleware in app.js

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import router from "./router";
 import db from "../config/db";
 import Logger from "../config/logger";
 import morganMiddleware from "./middleware/morganMiddleware";
+import { ErrorMiddleware } from "./middleware/error-middleware";
 
 // Configuração inicial do express
 const app = express();
@@ -16,6 +17,8 @@ app.use(morganMiddleware);
 
 // Importando as rotas e usando um prefixo
 app.use("/api/", router);
+
+app.use(ErrorMiddleware.handleError)
 
 // Definindo uma porta pra aplicação
 const port = config.get<number>("port");
